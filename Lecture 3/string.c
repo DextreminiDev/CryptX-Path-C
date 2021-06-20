00d0b853c105f0403c74d33ade49225b7d1dd06e
DextreminiDev <milindsharma242@gmail.com>
2021-06-20 15:50:18 +0530

    Add Lecture 3

---
+++ Lecture 3/string.c
@@ -0,0 +1,7 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+    string name = "Milind";
+    printf("%c\n", name[5]);
+}
---
+++ Lecture 3/string2.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+    string name = "Milind";
+
+
+    for(int i = 0; i < 6; i++){
+        printf("%c\n", name[i]);
+    }
+}
